9aa5d1d0e786c43cf6958b3e5d15f17f8dbb14e7
Tan 🔥 <tania.zuniga@pipefy.com>
2023-01-09 11:06:49 +0000

    Script to install docker swarm

--- install-docker.sh
+++ install-docker.sh
@@ -1,7 +1,7 @@
 echo -e "\n> Install docker"
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo apt-get install ca-certificates curl gnupg lsb-release net-tools -y
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -22,3 +22,13 @@ sudo chmod g+rwx "$HOME/.docker" -R
 
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
+echo -e "\n> Docker Swarm (should be dead but ok)"
+
+# For the "master"
+IP_MASTER=$(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+docker swarm init --advertise-addr IP_MASTER
+SWARM_TOKEN=$(docker swarm join-token -q worker)
+echo -e "\n RUN THIS ON NODES:"
+echo -e "docker swarm join --token $SWARM_TOKEN $IP_MASTER:2377"
+echo "export IP_MASTER=$(ip -f inet addr show eth1 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')" >> $HOME/.bashrc
+echo "export SWARM_TOKEN=$(docker swarm join-token -q worker)" >> $HOME/.bashrc
